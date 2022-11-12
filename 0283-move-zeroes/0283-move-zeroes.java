04bd663e70fa7ffa8a0ff9cd29503406cb792eba
Deva Anand <97371915+Deva-1903@users.noreply.github.com>
2022-11-12 11:55:39 +0530

    Time: 9 ms (34.51%), Space: 55.5 MB (7.70%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int zeros = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                zeros++;
+            }else {
+                if(zeros>0){
+                    int t = nums[i-zeros];
+                    nums[i-zeros] = nums[i];
+                    nums[i] = t;
+                }
+            }
+        }
+        
+    }
+}
