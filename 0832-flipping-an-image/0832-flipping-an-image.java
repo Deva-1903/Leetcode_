026fac6150c444f0d7b00ab4ecab5052bd5df8ae
Deva Anand <97371915+Deva-1903@users.noreply.github.com>
2022-11-15 00:29:51 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (90.17%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -2,8 +2,7 @@ class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         
         int start,end,temp;
-
-        
+ 
         for(int i=0; i<image.length; i++){ 
             start = 0; 
             end = image[i].length-1;
@@ -14,8 +13,7 @@ class Solution {
                 image[i][end] = temp;
                 start++;
                 end--;
-            } 
-            
+            }  
             for(int j=0; j<image[i].length; j++){
                 if(image[i][j]==0) image[i][j]=1;
                 else image[i][j]=0;
