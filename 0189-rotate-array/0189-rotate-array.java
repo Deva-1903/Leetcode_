9362417e5688a4e87c4a36779ae0070e6f4b5967
Deva Anand <97371915+Deva-1903@users.noreply.github.com>
2022-11-21 21:09:41 +0530

    Time: 1 ms (91.45%), Space: 64.7 MB (26.48%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,5 +1,10 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        // step1: reverse the array
+        // step2: split the array into two.
+        // step3: reverse both the two parts.
+        
+        //taking k modulo for edge cases
         k = k%nums.length;
         swap(nums, 0, nums.length-1);
         swap(nums, 0, k-1);
