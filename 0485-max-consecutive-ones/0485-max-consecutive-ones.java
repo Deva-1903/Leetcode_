250406d84599a09f1d2fb7786558927e9e0e736e
Deva Anand <97371915+Deva-1903@users.noreply.github.com>
2022-11-21 09:09:38 +0530

    Time: 3 ms (77.14%), Space: 57.9 MB (5.39%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -4,11 +4,9 @@ class Solution {
         int m2 = 0; 
         for(int i=0; i<nums.length; i++){    
             if(nums[i]==1){
-                m1+=1;    
+                m1++;    
             }else{
-                if(m2<m1){
-                    m2 = m1;
-                }
+                if(m2<m1) m2 = m1;
                 m1 = 0;
             }
         }
