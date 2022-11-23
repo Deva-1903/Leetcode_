9faad64757e63316ed2fc913d75dc26543942b85
Deva Anand <97371915+Deva-1903@users.noreply.github.com>
2022-11-23 23:57:09 +0530

    Time: 1 ms (81.24%), Space: 41.4 MB (74.67%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,13 +2,16 @@ class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         
+        //Initializie current row and previous row
         List<Integer> row, pre = null;
         for(int i=0; i<numRows; i++){
             row = new ArrayList<Integer>();
             for(int j=0; j<=i; j++){
+                //check if the pointer is in first or last column of the row.
                 if(j==0 || j==i){
                     row.add(1);
                 }else{
+                    //else go to previous row and sum the elements
                     row.add(pre.get(j - 1) + pre.get(j));
                 }               
             }
